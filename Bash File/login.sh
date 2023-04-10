74e0444e515337fc8a4e40d92a8fc3e8bcabca2c
Qii <57295427+syauqqii@users.noreply.github.com>
2023-04-10 18:18:55 +0700

    Create login.sh

---
+++ Bash File/login.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+
+getData=$(cat db.txt)
+rawData=($(echo $getData | awk -F '|' '{print $1,$2}'))
+
+echo ""
+read -p " [#] Input Username: " username
+read -p " [#] Input Password: " password
+
+userEncrypt=$(echo -n $username | md5sum | awk '{print $1}')
+passEncrypt=$(echo -n $password | md5sum | awk '{print $1}')
+
+if [ $userEncrypt == ${rawData[0]} ]; then
+        if [ $passEncrypt == ${rawData[1]} ]; then
+                echo -e "\n [~] Selamat Datang ${username}!"
+        else
+                echo -e "\n [!] Username / Password Salah!"
+        fi
+else
+        echo -e "\n [!] Username / Password Salah!"
+fi
