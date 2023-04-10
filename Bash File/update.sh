4e5e54f224d909f29dee38060cadb2d76dfb1c80
Qii <57295427+syauqqii@users.noreply.github.com>
2023-04-10 18:39:41 +0700

    Create update.sh

---
+++ Bash File/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+
+echo ""
+read -p " [#] Input New Username: " username
+read -p " [#] Input New Password: " password
+
+userEncrypt=$(echo -n $username | md5sum | awk '{print $1}')
+passEncrypt=$(echo -n $password | md5sum | awk '{print $1}')
+
+echo "${userEncrypt}|${passEncrypt}" > db.txt
