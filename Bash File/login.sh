6b3210c6383424bc321056467f8f284f6279caa1
Qii <57295427+syauqqii@users.noreply.github.com>
2023-04-13 23:18:31 +0700

    Update login.sh
    update

--- Bash File/login.sh
+++ Bash File/login.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-clear
+clearScreen() {
+    clear
+}
 
-rawData=($(cat db.txt | awk -F '|' '{print $1,$2}'))
+login() {
+    rawData=($(cat db.txt | awk -F '|' '{print $1,$2}'))
 
-echo ""
-read -p " [#] Input Username: " username
-read -p " [#] Input Password: " password
+    echo ""
+    read -p " [#] Input Username: " username
+    read -p " [#] Input Password: " password
+    
+    userEncrypt=$(echo -n $username | md5sum | awk '{print $1}')
+    passEncrypt=$(echo -n $password | md5sum | awk '{print $1}')
+    
+    if [ $userEncrypt == ${rawData[0]} ]; then
+            if [ $passEncrypt == ${rawData[1]} ]; then
+                    echo -e "\n [~] Selamat Datang ${username}!"
+            else
+                    echo -e "\n [!] Username / Password Salah!"
+            fi
+    else
+            echo -e "\n [!] Username / Password Salah!"
+    fi
+}
 
-userEncrypt=$(echo -n $username | md5sum | awk '{print $1}')
-passEncrypt=$(echo -n $password | md5sum | awk '{print $1}')
+main() {
+    clearScreen
+    login
+}
 
-if [ $userEncrypt == ${rawData[0]} ]; then
-        if [ $passEncrypt == ${rawData[1]} ]; then
-                echo -e "\n [~] Selamat Datang ${username}!"
-        else
-                echo -e "\n [!] Username / Password Salah!"
-        fi
-else
-        echo -e "\n [!] Username / Password Salah!"
-fi
+main
